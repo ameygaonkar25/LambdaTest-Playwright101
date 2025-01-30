0e907e2ff322556883e056a68c0533c162c72527
amey_gaonkar <ameygaonkar99@gmail.com>
2025-01-31 01:12:04 +0530

    commting changes and Ids file

--- src/test/java/Testcases/TestScenario2.java
+++ src/test/java/Testcases/TestScenario2.java
@@ -27,8 +27,8 @@ public class TestScenario2 {
         JsonObject capabilities = new JsonObject();
         JsonObject ltOptions = new JsonObject();
 
-        String user = "anuradha.bartonde";
-        String accessKey = "dT5fhgPKwpSjHH07NkK9H8X7mfRlcEvg9yALdYZyaoeGuZ7wTG";
+        String user = "ameygaonkar25";
+        String accessKey = "c0W23nYVo42qVXvUVIakuoKZb9bXDasNtAutqGb6sAHbsXKqcV";
 
         capabilities.addProperty("browsername", Browser); // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
         capabilities.addProperty("browserVersion",BrowserVersion);
